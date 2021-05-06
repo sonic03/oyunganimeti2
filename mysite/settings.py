c4617ccb3b80d1a7049096ce5d6b33524993dbe9
sonic03 <mrkayacik@yahoo.com>
2021-05-06 22:59:00 +0300

    sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,17 +92,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'denemedb',
+        #'NAME': 'denemedb',
 
-        'USER': 'postgres',
+        #'USER': 'postgres',
 
-        'PASSWORD': 'postgres',
+        #'PASSWORD': 'postgres',
 
-        'HOST': 'localhost',
+        #'HOST': 'localhost',
 
-        'PORT': '5432',
+        #'PORT': '5432',
     }
 }
 
